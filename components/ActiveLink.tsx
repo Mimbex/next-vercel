7c46a6c26d36c7cedd53a03dd49678f0eb539625
Dustin Mimbela <dmimbela@nlcode.com>
2022-03-06 18:40:16 -0500

    first commit

---
+++ components/ActiveLink.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { CSSProperties, FC } from "react";
+
+interface Props {
+  text: string;
+  href: string;
+}
+
+const style: CSSProperties = {
+  color: "#0070f3",
+  textDecoration: "underline",
+};
+
+export const ActiveLink: FC<Props> = ({ text, href }) => {
+  const { asPath } = useRouter();
+
+  return (
+    <Link href={href}>
+      <a style={asPath === href ? style : undefined}>{text}</a>
+    </Link>
+  );
+};
